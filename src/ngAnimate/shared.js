c91ec3b3fd7c860033ae5469a3ab051c3bfb2d20
Matias Niemelä <matias@yearofmoo.com>
2015-01-23 00:04:19 -0500

    refactor($animate): split drivers and include code for ngAnimateCSSDriver

---
+++ src/ngAnimate/shared.js
@@ -0,0 +1,32 @@
+var isArray = angular.isArray;
+var isString = angular.isString;
+var forEach = angular.forEach;
+var noop = angular.noop;
+
+function packageStyles(options) {
+  if (options && (options.to || options.from)) {
+    return {
+      to : options.to,
+      from : options.from
+    };
+  }
+}
+
+function packageResult(value, done) {
+  return { value : value, done : done };
+}
+
+function suffixClasses(classes, suffix) {
+  var className = '';
+  classes = isArray(classes)
+      ? classes
+      : classes && isString(classes) && classes.length
+          ? classes.split(/\s+/)
+          : [];
+  forEach(classes, function(klass, i) {
+    if (klass && klass.length > 0) {
+      className += (i > 0 ? ' ' : '') + klass + suffix;
+    }
+  });
+  return className;
+}
